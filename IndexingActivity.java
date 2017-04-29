8748dfd5d545cd56a3d7202f15f8cbe5bbd6a64c
spiral-nemesis <spiral-nemesis@users.noreply.github.com>
2017-04-29 22:56:26 +0200

    Wersja bez listy zdjęć, samo indeksowanie jednego

---
+++ IndexingActivity.java
@@ -0,0 +1,16 @@
+package com.example.jonaszrudnik.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IndexingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indexing);
+
+    }
+
+
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.jonaszrudnik.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+//KOMENTARZ DLA OLI - tu zamiast maina ma być menu zdjęć których jeszcze nie zindeksowano
 public class MainActivity extends AppCompatActivity {
+
+
+
+    Button button1;
+
+
+    //@Override
+    public void buttonOnClick(View v) {
+        // setIntent();
+        Intent i = new Intent(this, IndexingActivity.class);
+        startActivity(i);
+        //setContentView(R.layout.activity_main);
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Intialization Button
+
+       // button1 = (Button) findViewById(R.id.indexing);
+        //btnClickMe.setOnClickListener(MainActivity.this);
+        //onClick(btnClickMe);
+        //btnClickMe.setOnClickListener(MainActivity.btnClickMe);
+        //Here MainActivity.this is a Current Class Reference (context)
     }
 }
